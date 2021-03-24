6143c3cda49b3f423384cb7f2ecfd782078e838f
Thomas Bartelmess <tbartelmess@marketcircle.com>
2021-03-24 19:32:30 -0400

    Initial commit

---
+++ MyFramework/MyFrameworkTests/MyFrameworkTests.swift
@@ -0,0 +1,32 @@
+//
+//  MyFrameworkTests.swift
+//  MyFrameworkTests
+//
+//  Created by Thomas Bartelmess on 2021-03-24.
+//
+
+import XCTest
+@testable import MyFramework
+
+class MyFrameworkTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        XCTAssertTrue(false)
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
